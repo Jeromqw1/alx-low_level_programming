d3e906dab955b18ccdae231b2cc56dd016491eb4
Jeromqw1 <afayizu@gmail.com>
2023-02-16 11:47:38 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program.
+ * This program prints a message to the Standard output from the Main function.
  * return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
