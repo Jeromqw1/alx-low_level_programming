30afa78eda40f39ff4a011b7db5d88b48ac9384d
Jeromqw1 <afayizu@gmail.com>
2023-02-23 11:56:07 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,13 +22,16 @@ int main(void)
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
+			printf("FizzBuzz");
+		}
+		else if (i == 1)
+		{
 			printf("%d", i);
 		}
 		else
 		{
-			printf("%d", i);
+			print("%d", i);
 		}
-		printf("\n");
 	}
 	return (0);
 }
