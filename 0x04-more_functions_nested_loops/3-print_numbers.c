d3cb7b86fa2dcc05e25b96b01cde19a5fbdeb4ba
Jeromqw1 <afayizu@gmail.com>
2023-02-23 04:22:29 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - checks for a digit (0 through 9)
+ *
+ * Return: Always 0 (success)
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
