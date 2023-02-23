fbbdacd4e3d41215c065ab9abca69952d54c32e8
Jeromqw1 <afayizu@gmail.com>
2023-02-23 15:02:26 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,9 +16,7 @@ void print_number(int n)
 		_putchar('-');
 	}
 	k /= 10;
-	if (k != 0)
-	{
+	if  (k != 0)
 		print_number(k);
-		_putchar((unsigned int) n % 10 + '0');
-	}
+	_putchar((unsigned int) n % 10 + '0');
 }
