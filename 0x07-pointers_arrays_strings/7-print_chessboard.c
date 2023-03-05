5aa5252ab77a4f9091d8b7692943ef3b2c490eb7
Jeromqw1 <afayizu@gmail.com>
2023-03-05 07:50:04 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_chessboard - entry point
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+			_putchar('\n');
+		}
+	}
+}
