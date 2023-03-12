dddac0a523c6d7600ca77c858ff9a54c50a1d000
Jeromqw1 <afayizu@gmail.com>
2023-03-12 04:43:42 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - prints a string to the standard output stream
+ * puts - writes a string to the standard output stream, followed by a newline
+ * @s: pointer to the string to be printed
+ */
+void _puts(char *s)
+{
+	puts(s);
+}
