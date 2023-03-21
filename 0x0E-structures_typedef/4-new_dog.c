6dea224182914db940cde0ca303b5d137b1c2719
mclanc <judemclancy@gmail.com>
2023-03-21 00:20:07 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -67,6 +67,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 {
+	free(dog);
 	free(dog->name);
 	return (NULL);
 }
