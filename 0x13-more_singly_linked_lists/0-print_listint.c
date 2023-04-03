1ca425e6969b4f97856ade55236e1a3fb758b3d5
mclanc <judemclancy@gmail.com>
2023-04-03 23:25:26 +0000

    0-print_list.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
 * @h: A pointer to the head of the listint_t list.
 * Return: The number of nodes in the listint_t list.
 */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
