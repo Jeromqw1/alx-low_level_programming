d0c3795e47f91d658f407f69d2d69335c2571a19
mclanc <judemclancy@gmail.com>
2023-04-06 01:32:29 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -21,48 +21,43 @@ void free_listp(listp_t **head)
 		*head = NULL;
 	}
 }
-
 /**
- * print_listint_safe - prints a linked list.
- * @head: head of a list.
- *
- * Return: number of nodes in the list.
+ * free_listint_safe - frees a linked list.
+ * @h: pointer to the head of the list.
+ * Return: the size of the list that was free'd.
  */
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listp_t *visited_nodes, *new_node, *add;
+	size_t nnode = 0;
+	listp_t *current, *new, *add;
 
-	visited_nodes = NULL;
-	while (head != NULL)
+	current = NULL;
+	while (*h != NULL)
 	{
-		new_node = malloc(sizeof(listp_t));
-
-		if (new_node == NULL)
+		new = malloc(sizeof(listp_t));
+		if (new == NULL)
+		{
+			free_listp(&current);
 			exit(98);
+		}
+		new->p = (void *)*h;
+		new->next = current;
+		current = new;
 
-		new_node->p = (void *)head;
-		new_node->next = visited_nodes;
-		visited_nodes = new_node;
-
-		add = visited_nodes;
-
+		add = current;
 		while (add->next != NULL)
 		{
 			add = add->next;
-			if (head == add->p)
+			if (*h == add->p)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listint_t(&visited_nodes);
-				return (count);
+				free_listp(&current);
+				*h = NULL;
+				return (nnode);
 			}
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		count++;
 	}
-
-	free_listint_t(&visited_nodes);
-	return (count);
+	*h = (*h)->next;
+	free(new->p);
+	free(new);
+	nnode++;
 }
